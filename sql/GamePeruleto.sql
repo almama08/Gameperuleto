be19282551d6caf06669b0260278e6ee6a0f4343
Alex Martínez <almama08@alumnatflorida.es>
2026-05-06 16:53:48 +0200

    cambios en la base de datos

--- sql/GamePeruleto.sql
+++ sql/GamePeruleto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: db
--- Tiempo de generación: 23-04-2026 a las 08:41:01
+-- Tiempo de generación: 06-05-2026 a las 14:52:44
 -- Versión del servidor: 9.6.0
 -- Versión de PHP: 8.3.30
 
@@ -37,6 +37,18 @@ CREATE TABLE `juegos` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `progreso`
+--
+
+CREATE TABLE `progreso` (
+  `id_usuario` int NOT NULL,
+  `id_videojuego` int NOT NULL,
+  `completado` tinyint(1) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -55,6 +67,13 @@ CREATE TABLE `usuarios` (
 --
 ALTER TABLE `juegos`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `progreso`
+--
+ALTER TABLE `progreso`
+  ADD PRIMARY KEY (`id_usuario`,`id_videojuego`),
+  ADD KEY `progreso_id_videojuego` (`id_videojuego`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -78,6 +97,17 @@ ALTER TABLE `juegos`
 --
 ALTER TABLE `usuarios`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `progreso`
+--
+ALTER TABLE `progreso`
+  ADD CONSTRAINT `progreso_id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  ADD CONSTRAINT `progreso_id_videojuego` FOREIGN KEY (`id_videojuego`) REFERENCES `juegos` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
